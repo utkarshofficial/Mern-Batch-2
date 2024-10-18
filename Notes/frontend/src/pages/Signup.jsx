154193dd7c64861a392973eb2d5d1c9218309ffd
Utkarsh Sharma <83834666+utkarshofficial@users.noreply.github.com>
2024-10-18 08:16:03 +0530

    Added frontend and cleanup, also created a signup page

---
+++ Notes/frontend/src/pages/Signup.jsx
@@ -0,0 +1,87 @@
+import { useState } from "react";
+import axios from 'axios';
+
+const Signup = () => {
+  const [fullName, setFullName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
+  const [isPasswordMatched, setIsPasswordMatched] = useState(true)
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if(password != confirmPassword){
+      setIsPasswordMatched(false)
+      return
+    }
+    setIsPasswordMatched(true)
+
+    try{
+      const apiURL = "http://localhost:5000/api/auth/register"
+      const response = axios.post(apiURL, {
+        fullName,
+        email,
+        password
+      })
+
+      console.log(response)
+    }
+    catch(err){
+      console.log(err)
+    }
+  };
+
+  return (
+    <div>
+      <h1>Signup</h1>
+      <form>
+        <div>
+          <label htmlFor="full_name">Full Name</label>
+          <input
+            type="text"
+            placeholder="Full Name"
+            value={fullName}
+            onChange={(e) => setFullName(e.target.value)}
+          />
+        </div>
+        <div>
+          <label htmlFor="email">Email</label>
+          <input
+            type="email"
+            placeholder="Email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+        </div>
+        <div>
+          <label htmlFor="password">Password</label>
+          <input
+          className={`${!isPasswordMatched?"input-red":""}`}
+            type="password"
+            placeholder="********"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+        </div>
+        <div>
+          <label htmlFor="confirm_password">Confirm Password</label>
+          <input
+          className={`${!isPasswordMatched?"input-red":""}`}
+            type="password"
+            placeholder="********"
+            value={confirmPassword}
+            onChange={(e) => setConfirmPassword(e.target.value)}
+          />
+        </div>
+        <button onClick={handleSubmit} type="submit">
+          Submit
+        </button>
+        <p>
+          Already have an account <a href="">Log in</a>
+        </p>
+      </form>
+    </div>
+  );
+};
+
+export default Signup;
