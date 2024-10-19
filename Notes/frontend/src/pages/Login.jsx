a4710f6bbfdb6b2f7da7ce5c54fa6dd1f3ab93f6
Utkarsh Sharma <83834666+utkarshofficial@users.noreply.github.com>
2024-10-19 09:06:27 +0530

    Log in page completed with token generation

--- Notes/frontend/src/pages/Login.jsx
+++ Notes/frontend/src/pages/Login.jsx
@@ -1,9 +1,63 @@
-import React from 'react'
+import { useState } from "react";
+import axios from 'axios';
+import { Link, useNavigate } from "react-router-dom";
 
 const Login = () => {
-  return (
-    <div>Login</div>
-  )
-}
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const navigate = useNavigate()
 
-export default Login
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    try{
+      const apiURL = "http://localhost:5000/api/auth/login"
+      const response = await axios.post(apiURL, {
+        email,
+        password
+      })
+      
+      if(response.data.success){
+        localStorage.setItem('token', response.data.token);
+        navigate("/")
+      }
+    }
+    catch(err){
+      console.log(err)
+    }
+  };
+
+  return (
+    <div>
+      <h1>Login</h1>
+      <form>
+        <div>
+          <label htmlFor="email">Email</label>
+          <input
+            type="email"
+            placeholder="Email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+        </div>
+        <div>
+          <label htmlFor="password">Password</label>
+          <input
+            type="password"
+            placeholder="********"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+        </div>
+        <button onClick={handleSubmit} type="submit">
+          Login
+        </button>
+        <p>
+          Create an account <Link to={"/signup"}>Sign up</Link>
+        </p>
+      </form>
+    </div>
+  );
+};
+
+export default Login;
--- Notes/server/routes/auth.js
+++ Notes/server/routes/auth.js
@@ -1,6 +1,7 @@
 import express from "express";
 import User from "../models/User.js";
 import bcrypt from "bcrypt";
+import jwt from 'jsonwebtoken'
 
 const router = express.Router();
 
@@ -12,28 +13,84 @@ router.post("/register", async (request, response) => {
     const user = await User.findOne({ email });
 
     if (user) {
-      return response.status(400).json({ message: "Email already exists" });
+      return response
+        .status(400)
+        .json({ success: false, message: "Email already exists" });
     }
 
     // encrypt the password
     const hashedPassword = await bcrypt.hash(password, 10);
 
     // create new user with user data
-    const newUser = new User({ 
-        full_name, 
-        email, 
-        password: hashedPassword 
+    const newUser = new User({
+      full_name,
+      email,
+      password: hashedPassword,
     });
 
-    await newUser.save()
+    await newUser.save();
 
-    return response.status(200).json({success: true, message: "Account is created successfully"})
+    return response
+      .status(200)
+      .json({ success: true, message: "Account is created successfully" });
 
   } catch (error) {
-    console.log(error)
+    return response
+      .status(500)
+      .json({ 
+        success: false,
+        message: "Error in adding user" 
+      });
   }
 });
 
+router.post("/login", async (request, response) => {
+  try {
+    // get  the user data from the request body
+    const { email, password } = request.body;
+    // email is already or not
+    const user = await User.findOne({ email });
 
+    if (!user) {
+      return response
+        .status(401)
+        .json({ success: false, message: "User does not exists" });
+    }
 
-export default router
+    // * Compare the password with database
+    const isMatch = await bcrypt.compare(password, user.password);
+    if (!isMatch) {
+      return response
+        .status(401)
+        .json({ success: false, message: "User or password is incorrect" });
+    }
+
+    // * Generate token
+    const secretKey = process.env.SECRET_KEY
+    const token = jwt.sign(
+      {id: user._id},
+      secretKey,
+      {expiresIn: "24h"}
+    )
+
+    // send response user to client
+    return response
+      .status(200)
+      .json({ 
+        success: true,
+        token,
+        user: {name: user.full_name}, 
+        message: "Login successfully" 
+      });
+
+  } catch (error) {
+    return response
+      .status(500)
+      .json({ 
+        success: false,
+        message: "Error in login" 
+      });
+  }
+});
+
+export default router;
