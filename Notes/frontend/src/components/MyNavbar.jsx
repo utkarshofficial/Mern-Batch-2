8c81513b8f8f4e3e1f97884db4d73f63dbc9c008
Utkarsh Sharma <83834666+utkarshofficial@users.noreply.github.com>
2024-10-24 09:04:49 +0530

    Navbar style added

--- Notes/frontend/src/components/MyNavbar.jsx
+++ Notes/frontend/src/components/MyNavbar.jsx
@@ -1,36 +1,31 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import { useAuth } from "../context/ContextProvider";
+import Container from "react-bootstrap/Container";
+import Navbar from "react-bootstrap/Navbar";
 
 const MyNavbar = () => {
   const { user } = useAuth();
   return (
-    <div>
-      <ul>
-        {!user ? (
-          <div>
-            <li>
-              <Link to="/signup">Signup</Link>
-            </li>
-            <li>
-              <Link to="/login">Login</Link>
-            </li>
-          </div>
-        ) : (
-          <div>
-            <li>
-              <Link to="/home">Home</Link>
-            </li>
-            <li>
-              <button>{user.name}</button>
-            </li>
-            <li>
-              <Link to="/logout">Logout</Link>
-            </li>
-          </div>
-        )}
-      </ul>
-    </div>
+    <Navbar className="bg-body-tertiary">
+      <Container>
+        <Navbar.Brand href="#home">Navbar with text</Navbar.Brand>
+        <Navbar.Toggle />
+        <Navbar.Collapse className="justify-content-end">
+          {!user ? (
+            <>
+              <Link className="m-1" to="/signup">Signup</Link>
+              <Link className="m-1" to="/login">Login</Link>
+            </>
+          ) : (
+            <div>
+              <Navbar.Text className="m-1" >Signed in as: <span className="text-danger">{user.name.toUpperCase()}</span></Navbar.Text>
+              <Link className="m-1"  to="/logout">Logout</Link>
+            </div>
+          )}
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
   );
 };
 
