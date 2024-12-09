9c9178401858aac9efed13b433fb59f4c9259c3d
Utkarsh Sharma <83834666+utkarshofficial@users.noreply.github.com>
2024-12-09 22:29:09 +0530

    Popup Use state added

--- React/task-list/src/components/TaskCard.jsx
+++ React/task-list/src/components/TaskCard.jsx
@@ -1,8 +1,26 @@
 import PropTypes from "prop-types";
+import Popup from "./Popup";
+import { useState } from "react";
 
-const TaskCard = ({ task, handleDelete }) => {
+const TaskCard = ({ task, handleDelete, handleUpdate }) => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const handleOpen = () => setIsOpen(true);
+  const handleClose = () => setIsOpen(false);
+
+
+
   return (
-    <div className="task-card text-white m-3 p-3 bg-[#5E503F] rounded-lg">
+    <>
+      { isOpen && (
+        <Popup 
+          isOpen={isOpen}
+          handleClose={handleClose}
+          task={task}
+          handleSubmit={handleUpdate}
+        />
+      ) }
+      <div className="task-card text-white m-3 p-3 bg-[#5E503F] rounded-lg">
       <div className="task-header flex justify-between">
         <p className="text-xs font-bold">Task Description</p>
         <span className="text-xs">{task.time}</span>
@@ -13,19 +31,21 @@ const TaskCard = ({ task, handleDelete }) => {
       <div className="action-buttons flex justify-between">
         <input type="checkbox" name="completed" checked={task.completed} />
         <div className="flex gap-2">
-          <button className="edit font-bold px-3 rounded-md bg-[#22333B]">Edit</button>
+          <button className="edit font-bold px-3 rounded-md bg-[#22333B]" onClick={handleOpen}>Edit</button>
           <button className="delete font-bold px-3 rounded-md bg-[#22333B]" onClick={() => handleDelete(task.id)}>
             Delete
           </button>
         </div>
       </div>
     </div>
+    </>
   );
 };
 
 TaskCard.propTypes = {
   task: PropTypes.object,
   handleDelete: PropTypes.func,
+  handleUpdate: PropTypes.func,
 };
 
 export default TaskCard;
